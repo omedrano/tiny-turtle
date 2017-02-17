3f2f166522314a773fadf3a2780409d156b0dacf
Osmairy Medrano <omedrano@cristoreyny.org>
2017-02-17 14:40:06 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,24 @@
+TinyTurtle.apply(window);
+function square(width) {
+forward(width);
+left(90);
+forward(width);
+left(90);
+forward(width);
+left(90);
+forward(width);
+
+}
+square(50);
+square(50);
+square(30);
+square(30);
+square(20);
+square(20);
+square(10);
+square(10);
+square(100);
+square(100);
+
+
+
